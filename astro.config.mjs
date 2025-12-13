d01859dda1fefc6ab012fc82077c78b50dd50ae2
stephanofer <slowdown_4@hotmail.com>
2025-12-13 04:46:31 -0500

    fix: update Open Graph image URL and correct description for sanctions page

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
+  site: 'https://sanciones.mycraft.es',
   vite: {
     plugins: [tailwindcss()]
   },
